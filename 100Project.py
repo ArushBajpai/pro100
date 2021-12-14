b6b096026bdfbfc8256f5bfa81c679ce83a36217
ArushBajpai <74227900+ArushBajpai@users.noreply.github.com>
2021-12-14 22:42:55 +0530

    Add files via upload

---
+++ 100Project.py
@@ -0,0 +1,16 @@
+class Atm :
+    def __init__(self,cardNumber,pinNumber):
+        self.cardNumber = cardNumber
+        self.pinNumber = pinNumber 
+    def CashWithdrawl (self):
+        print("Cash withdrawl")
+    
+    def BalanceEnquiry (self):
+        print("BalanceEnquiry")
+
+    def CreditCard (self):
+        print("CreditCard")
+
+    
+
+    
